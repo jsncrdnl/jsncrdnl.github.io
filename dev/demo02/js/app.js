1bcd37fcad8f825586350db6dbe397b1bebdc687
unknown <jsn.crdnl@gmail.com>
2015-02-06 11:57:10 +0100

    m

--- dev/demo02/js/app.js
+++ dev/demo02/js/app.js
@@ -1,5 +1,5 @@
 
-	var app = angular.module("twocan-app", ["ngRoute"]);
+	var app = angular.module("twocan-app", ["ngRoute", "ngAnimate", "textAngular"]);
 	
 	app.config(function($routeProvider) {
 		$routeProvider
@@ -7,7 +7,7 @@
 			// route for the home page
 			.when('/', {
 				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
+				controller  : 'homeController'
 			})
 
 			// route for the about page
@@ -28,32 +28,39 @@
 	
 	app.controller("mainController", function($scope)
 	{
-		// $scope.editDisabled = true;
-		// $scope.customStyle= ".ta-toolbar{ display:none; }";
-	
-		// $scope.toggleActiveEditor = function(){
-			// if ( $scope.editDisabled )
-			// {
-				// $scope.editDisabled = false;
-				// $scope.customStyle= "";				
-			// }
-			// else
-			// {
-				// $scope.editDisabled = true;
-				// $scope.customStyle= ".ta-toolbar{ display:none; }";
-			// }
-		// };
 		
+		// WYSIWYG
+		$scope.editDisabled = true;
+		$scope.customStyle= ".ta-toolbar{ display:none; }";	
+		$scope.toggleActiveEditor = function(){
+			if ( $scope.editDisabled )
+			{
+				$scope.editDisabled = false;
+				$scope.customStyle= "";				
+			}
+			else
+			{
+				$scope.editDisabled = true;
+				$scope.customStyle= ".ta-toolbar{ display:none; }";
+			}
+		};
+		/////////
+		
+	});
+	
+	
+	app.controller('homeController', function($scope) {
+		$scope.pageClass = 'page-home';
 		$scope.message = 'This is the home page !';
 	});
 	
-	
 	app.controller('aboutController', function($scope) {
+		$scope.pageClass = 'page-about';
 		$scope.message = 'Look! I am an about page.';
-	});
-	
+	});	
 	
 	app.controller('contactController', function($scope) {
+		$scope.pageClass = 'page-contact';
 		$scope.message = 'Contact us! JK. This is just a demo.';
 	});
 	
