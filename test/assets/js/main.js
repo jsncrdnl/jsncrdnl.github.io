e007732900f00244b5b55bd66d71389471ec2716
unknown <jsn.crdnl@gmail.com>
2015-06-30 12:42:06 +0200

    a

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -13,36 +13,34 @@ var serializeTwocan = function (){
 //  alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
 //};
 
-var homepage = function(){
-    console.log("home");
-    $("body").stop().animate({"background-color": "white"}, 1000);
-};
-var page02 = function(){
-    console.log("page02");
-    $("body").stop().animate({"background-color": "blue"}, 1000);
-};
-var page03 = function(){
-    console.log("page03");
-    $("body").stop().animate({"background-color": "red"}, 1000);
-};
-var page04 = function(){
-    console.log("page04");
-    $("body").stop().animate({"background-color": "green"}, 1000);
-};
-var page404 = function(){
-    console.log("page404");
-    $("body").stop().animate({"background-color": "black"}, 1000);
-};
+//var homepage = function(){
+//    console.log("home");
+//    $("body").stop().animate({"background-color": "white"}, 1000);
+//};
+//var page02 = function(){
+//    console.log("page02");
+//    $("body").stop().animate({"background-color": "blue"}, 1000);
+//};
+//var page03 = function(){
+//    console.log("page03");
+//    $("body").stop().animate({"background-color": "red"}, 1000);
+//};
+//var page04 = function(){
+//    console.log("page04");
+//    $("body").stop().animate({"background-color": "green"}, 1000);
+//};
+//var page404 = function(){
+//    console.log("page404");
+//    $("body").stop().animate({"background-color": "black"}, 1000);
+//};
 
 
 $(document).ready(function()
 {
-    page('/test/',              homepage);
-    page('/test/page02.html',   page02);
-    page('/test/page03.html',   page03);
-    page('/test/page04.html',   page04);
-    page('*',                   page404);
-    page();
+//    page('/test/',              homepage);
+//    page('/test/page02.html',   page02);
+//    page('/test/page03.html',   page03);
+//    page('/test/page04.html',   page04);
 
     $("*[twocan-input]").each(function(index,value){
         updateTwocan( $(this) );
@@ -55,27 +53,26 @@ $(document).ready(function()
             $("body").addClass("reloadingContent");
             var tmplink = $(this).attr("href");
             var pagepath = "/test" + tmplink;
-            var asyncLoad = $.get( pagepath )
-                .done(function(data){
-                    $("body").addClass("contentReloaded");
-                    $("div[twocan-content]").html( $(data).find("div[twocan-content]").html() );
-//                    window.history.pushState("string", $(data).find("title").text(), pagepath );
-                    page( pagepath );
 
-//                    var scriptpath = "/test/assets/js" + tmplink.replace(".html",".js");
-//                    if(tmplink!=null && tmplink=="/") scriptpath =  "/test/assets/js/index.js";
-//                    $.getScript( scriptpath )
-//                        .done(function(){ console.log("script loaded"); })
-//                        .fail(function(){ console.log("script NOT loaded"); });
-                })
-                .fail(function(){
-//                  window.location = tmplink;
-                    console.log("an error occured while loading page : should redirect to page instead of loading it");
-                })
-            .always(function(){
-                $("body").removeClass("reloadingContent");
+            page( pagepath, function()
+            {
+                var asyncLoad = $.get( pagepath )
+                    .done(function(data){
+                        $("body").addClass("contentReloaded");
+                        $("div[twocan-content]").html( $(data).find("div[twocan-content]").html() );
+                    })
+                    .fail(function(){
+                        //window.location = tmplink;
+                        console.log("an error occured while loading page : should redirect to page instead of loading it");
+                    })
+                    .always(function(){
+                        $("body").removeClass("reloadingContent");
+                    });
             });
             e.preventDefault();
         });
     });
+
+    page('*', page404);
+    page();
 });
