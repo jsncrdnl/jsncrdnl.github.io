5127777ade6892a77135bacf738645f1130ae58f
unknown <jsn.crdnl@gmail.com>
2015-06-30 11:01:39 +0200

    z

--- test/assets/js/index.js
+++ test/assets/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function()
 {
-    $("#mainContent").stop().animate({color: "black"}, 1000);
+    $("#mainContent").stop().animate({color: "blue"}, 1000);
+    console.log("idx script !!");
 });
