5ea122a5d7e088e80fae701e6274ae6b8f0338f9
Jason_TWOCAN <jason@twocan.be>
2016-11-24 19:28:30 +0100

    a

--- dev/dabati/js/plume-nav.js
+++ dev/dabati/js/plume-nav.js
@@ -151,7 +151,7 @@ var initplumenavContent = function()
         });
 
         $(this).click(function(e){
-            console.log( "e.currentTarget = ", e.currentTarget);
+            console.log( "e.currentTarget = ", e, e.currentTarget);
             bodyLoading();
 
 
